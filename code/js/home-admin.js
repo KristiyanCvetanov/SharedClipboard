d7fd919668212a0f1c0c732850ba0179958fe16a
Daniel Velichkov <daniel.velichkov@sap.com>
2023-02-01 17:17:13 +0200

    Add user to clipboard done

--- code/js/home-admin.js
+++ code/js/home-admin.js
@@ -1,4 +1,4 @@
-let user_id, clipboard_id;
+let clipboard_id;
 
 function unhide_subscription_area(user, clipboard) {
     const hidden_form = document.getElementById("subscription_area");
@@ -6,11 +6,15 @@ function unhide_subscription_area(user, clipboard) {
 
     hidden_form.removeAttribute("hidden");
 
-    user_id = user;
     clipboard_id = clipboard;
 }
 
 async function add_user_to_clipboard() {
+    console.log(clipboard_id);
+
+    let user_id = document.getElementById("userInSubscriptionArea").value;
+
+    console.log(user_id);
     let formData = new FormData();
     formData.append("user_id", user_id)
     formData.append("clipboard_id", clipboard_id);
